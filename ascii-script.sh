14ca8faeec7ab455d6f2a79c9ead474eab1c7766
rakeshpatra <147680316+Rakesh-Patra@users.noreply.github.com>
2026-03-02 22:35:10 +0530

    execution command written

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update -y
 sudo apt-get install cowsay -y
-cowsay -f cow "Run for cover, I am a COW....MOO" >> cow.txt
+cowsay "Run for cover, I am a COW....MOO" >> cow.txt
 grep -i "cow" cow.txt
 cat cow.txt
 ls -ltra
