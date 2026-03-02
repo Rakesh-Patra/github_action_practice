6e2ee4c05b4cd18898b6f0153cd09fe884357a8e
rakeshpatra <147680316+Rakesh-Patra@users.noreply.github.com>
2026-03-02 20:49:31 +0530

    change from dragon to cow

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update -y
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+cowsay -f cow "Run for cover, I am a cow....RAWR" >> cow.txt
 grep -i "dragon" dragon.txt
-cat dragon.txt
+cat cow.txt
 ls -ltra
