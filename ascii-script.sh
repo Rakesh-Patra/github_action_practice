253f336942b1c73665c3892f21ac312506787844
rakeshpatra <147680316+Rakesh-Patra@users.noreply.github.com>
2026-03-02 20:52:04 +0530

    change

--- ascii-script.sh
+++ ascii-script.sh
@@ -2,6 +2,6 @@
 sudo apt-get update -y
 sudo apt-get install cowsay -y
 cowsay -f cow "Run for cover, I am a cow....RAWR" >> cow.txt
-grep -i "dragon" dragon.txt
+grep -i "dragon" cow.txt
 cat cow.txt
 ls -ltra
